cd3a16ec55319fd88b7c17233e8991386977198a
ZiruiOu <zirui.ou@pku.edu.cn>
2023-03-14 19:42:29 +0800

    feat: add encode_decode test

--- THHyperIBLT/__init__.py
+++ THHyperIBLT/__init__.py
@@ -7,10 +7,6 @@ MAX_PRIME = (2**61) - 1
 
 __all__ = ['THHyperIBLT']
 
-def _md5_hashbuckets(num_row: int, numel: int, num_col: int):
-    pass
-
-
 class THHyperIBLT(object):
     def __init__(self, num_row, num_col, numel, num_block):
         self.num_row: int = num_row
@@ -28,7 +24,7 @@ class THHyperIBLT(object):
         # Precompute hash bucket
         rng_state = torch.random.get_rng_state()
         # TODO (ozr): parameter
-        torch.random.manual_seed(42)
+        torch.random.manual_seed(114514)
 
         self.hash_bucket = torch.randint(
             0, num_col, size=(num_row, numel)
@@ -46,14 +42,14 @@ class THHyperIBLT(object):
         self.value_sum = self.value_sum.to(self.device)
         self.counter = self.counter.to(self.device)
     
-        numel_per_block = self.numel // self.num_block
+        numel_per_block = index.numel() // self.num_block
         if self.numel % self.num_block != 0:
             numel_per_block += 1
     
         for row in range(self.num_row):
             for i in range(self.num_block):
                 block_start = numel_per_block * i
-                block_end = min(block_start + numel_per_block, self.numel)
+                block_end = min(numel_per_block * (i + 1), self.numel)
     
                 block_idx = self.hash_bucket[row,block_start:block_end].clone()
                 block_idx = block_idx.long().to(self.device)
