a4e136ef905378d406d1c1eae84cf788822d0070
ZiruiOu <zirui.ou@pku.edu.cn>
2023-05-24 16:48:54 +0000

    fix(bind.cc): fix up pure bucket identification function

--- bind.cc
+++ bind.cc
@@ -8,158 +8,169 @@
 
 namespace py = pybind11;
 
-class HyperIBLTDecoder {
- public:
-  HyperIBLTDecoder(int numRow, int numCol, int numel, uint32_t modulo,
-                   py::array_t<int64_t> keySum, py::array_t<float> valSum,
-                   py::array_t<int32_t> counter,
-                   py::array_t<int32_t> hashBuckets)
-      : numRow(numRow),
-        numCol(numCol),
-        numel(numel),
-        modulo(modulo),
-        keySum(keySum),
-        valSum(valSum),
-        counter(counter),
-        hashBuckets(hashBuckets) {}
+extern "C" {
+int hyperiblt_decode(int numRow, int numCol, int64_t numel, uint32_t modulo,
+                     py::array_t<int64_t> keySum, py::array_t<float> valSum,
+                     py::array_t<int32_t> counter,
+                     py::array_t<int64_t> hashBuckets,
+                     py::array_t<float> results) {
+  // std::cout << "Hello from hyperiblt_decode" << std::endl;
+  auto keySumProxy = keySum.mutable_unchecked<2>();     // (numRow, numCol)
+  auto valSumProxy = valSum.mutable_unchecked<2>();     // (numRow, numCol)
+  auto counterProxy = counter.mutable_unchecked<2>();   // (numRow, numCol)
+  auto hashProxy = hashBuckets.mutable_unchecked<2>();  // (numRow, numNumel)
+  auto resultProxy = results.mutable_unchecked<1>();
+  // std::cout << "Ok ... " << std::endl;
 
-  ~HyperIBLTDecoder() = default;
+  auto isPureBucket = [&](int i, int j, int64_t &key) {
+    int32_t cnt = counterProxy(i, j);
+    if (keySumProxy(i, j) % cnt != 0) {
+      return false;
+    }
+    key = keySumProxy(i, j) / cnt;
 
-  bool decode(py::array_t<float> results) {
-    auto keySumProxy = keySum.mutable_unchecked<2>();     // (numRow, numCol)
-    auto valSumProxy = valSum.mutable_unchecked<2>();     // (numRow, numCol)
-    auto counterProxy = counter.mutable_unchecked<2>();   // (numRow, numCol)
-    auto hashProxy = hashBuckets.mutable_unchecked<2>();  // (numRow, numNumel)
-    auto resultProxy = results.mutable_unchecked<1>();
+    // NOTE (ozr): may be buggy here ...
+    if (key >= numel || key < 0) {
+      return false;
+    }
+    if (key < numel && hashProxy(i, key) != j) {
+      return false;
+    }
+    return true;
+  };
 
-    auto isPureBucket = [&](int i, int j, uint32_t &key) {
-      uint32_t cnt = counterProxy(i, j);
-      if (keySumProxy(i, j) % cnt != 0) {
-        return false;
-      }
-      key = keySumProxy(i, j) / cnt;
-      if (key < (uint32_t)numel && hashProxy(i, key) != j) {
-        return false;
-      }
-      return true;
-    };
+  auto removeFromSlot = [&keySumProxy, &valSumProxy, &counterProxy](
+                            int r, int c, int pureRow, int pureCol) {
+    keySumProxy(r, c) -= keySumProxy(pureRow, pureCol);
+    valSumProxy(r, c) -= valSumProxy(pureRow, pureCol);
+    counterProxy(r, c) -= counterProxy(pureRow, pureCol);
+  };
 
-    auto removeFromSlot = [&keySumProxy, &valSumProxy, &counterProxy](
-                              int r, int c, int pureRow, int pureCol) {
-      keySumProxy(r, c) = keySumProxy(r, c) - keySumProxy(pureRow, pureCol);
-      valSumProxy(r, c) -= valSumProxy(pureRow, pureCol);
-      counterProxy(r, c) -= counterProxy(pureRow, pureCol);
-    };
+  // std::cout << "generating candidates: numRow: " << numRow
+  //           << "numCol: " << numCol << std::endl;
 
-    std::queue<int> *candidates = new std::queue<int>[numRow];
-    std::vector<std::vector<bool>> visited(numRow);
-    for (int i = 0; i < numRow; i++) {
-      visited[i].resize(numCol);
-      for (int j = 0; j < numCol; j++) {
-        visited[i][j] = false;
+  std::queue<int> *candidates = new std::queue<int>[numRow];
+  std::vector<std::vector<bool>> visited(numRow);
+  for (int i = 0; i < numRow; i++) {
+    visited[i].resize(numCol);
+    for (int j = 0; j < numCol; j++) {
+      visited[i][j] = false;
+    }
+  }
+
+  // std::cout << "generating candidates: Ok ..." << std::endl;
+
+  int64_t key = 0;
+
+  for (int i = 0; i < numRow; i++) {
+    for (int j = 0; j < numCol; j++) {
+      if (counterProxy(i, j) == 0) {
+        // std::cout << "searching for (" << i << ", " << j << ")" << std::endl;
+        // std::cout << "visited " << std::endl;
+        visited[i][j] = true;
+      } else if (isPureBucket(i, j, key)) {
+        // std::cout << "searching for (" << i << ", " << j << ")" << std::endl;
+        // std::cout << "pure bucket with key = " << key << std::endl;
+        visited[i][j] = true;
+        resultProxy(key) += valSumProxy(i, j);
+
+        for (int k = 0; k < numRow; k++) {
+          if (k == i) {
+            continue;
+          }
+
+          int64_t pos = hashProxy(k, key);
+          removeFromSlot(k, pos, i, j);
+
+          candidates[k].push(pos);
+        }
+
+        removeFromSlot(i, j, i, j);
       }
     }
+  }
 
-    uint32_t key = 0;
+  bool stop = true;
+  while (true) {
+    stop = true;
+
+    // std::cout << "Hello from Loop..." << std::endl;
 
     for (int i = 0; i < numRow; i++) {
-      for (int j = 0; j < numCol; j++) {
+      while (!candidates[i].empty()) {
+        int j = candidates[i].front();
+        candidates[i].pop();
+
+        // std::cout << "Row " << i << " : current candidate: " << j
+        //           << " numcol: " << numCol << std::endl;
+
+        if (visited[i][j]) {
+          // std::cout << "candidate: " << j << " visited " << std::endl;
+          continue;
+        }
+
+        // std::cout << "checking counterProxy: " << j << std::endl;
         if (counterProxy(i, j) == 0) {
           visited[i][j] = true;
-        } else if (isPureBucket(i, j, key)) {
+          continue;
+        }
+
+        // std::cout << "visiting pure bucket " << j << std::endl;
+        // std::cout << "visiting pure bucket " << j
+        //           << " value = " << keySumProxy(i, j) / counterProxy(i, j)
+        //           << std::endl;
+
+        if (isPureBucket(i, j, key)) {
           visited[i][j] = true;
+          // std::cout << "current key" << key << std::endl;
+
           resultProxy(key) += valSumProxy(i, j);
 
           for (int k = 0; k < numRow; k++) {
-            if (k == i) {
-              continue;
-            }
-            uint32_t pos = hashProxy(k, key);
-            removeFromSlot(k, pos, i, j);
+            if (k == i) continue;
+            int64_t pos = hashProxy(k, key);
 
+            // std::cout << "Moving " << pos << " to candidates." << std::endl;
+
+            removeFromSlot(k, pos, i, j);
             candidates[k].push(pos);
           }
 
           removeFromSlot(i, j, i, j);
         }
+
+        // std::cout << "A fail key: " << key << std::endl;
       }
     }
 
-    bool stop = true;
-    while (true) {
-      stop = true;
-
-      for (int i = 0; i < numRow; i++) {
-        if (!candidates[i].empty()) {
-          stop = false;
-        }
-
-        while (!candidates[i].empty()) {
-          int j = candidates[i].front();
-          candidates[i].pop();
-
-          if (visited[i][j]) {
-            continue;
-          }
-
-          if (counterProxy(i, j) == 0) {
-            visited[i][j] = true;
-          } else if (isPureBucket(i, j, key)) {
-            visited[i][j] = true;
-            // TODO : insertion
-
-            resultProxy(key) += valSumProxy(i, j);
-
-            for (int k = 0; k < numRow; k++) {
-              if (k == i) continue;
-              uint32_t pos = hashProxy(k, key);
-
-              removeFromSlot(k, pos, i, j);
-              candidates[k].push(pos);
-            }
-
-            removeFromSlot(i, j, i, j);
-          }
-        }
-      }
-
-      if (stop) {
-        break;
-      }
-    }
-
-    delete[] candidates;
-
-    int failCnt = 0;
     for (int i = 0; i < numRow; i++) {
-      for (int j = 0; j < numCol; j++) {
-        if (!visited[i][j]) {
-          failCnt++;
-        }
+      if (!candidates[i].empty()) {
+        stop = false;
       }
     }
 
-    std::cout << "number of fail counts: " << failCnt << std::endl;
-    return (failCnt == 0);
+    if (stop) {
+      break;
+    }
   }
 
- private:
-  int numRow;
-  int numCol;
-  int numel;
-  uint32_t modulo;
-  py::array_t<int64_t> keySum;
-  py::array_t<float> valSum;
-  py::array_t<int32_t> counter;
-  py::array_t<int32_t> hashBuckets;
-};
+  int failCnt = 0;
+  for (int i = 0; i < numRow; i++) {
+    for (int j = 0; j < numCol; j++) {
+      if (!visited[i][j]) {
+        failCnt++;
+      }
+    }
+  }
+
+  delete[] candidates;
+
+  return failCnt;
+}
+}
 
 PYBIND11_MODULE(fed_iblt, m) {
   m.doc() = "HyperIBLT by pybind11";
 
-  pybind11::class_<HyperIBLTDecoder>(m, "HyperIBLTDecoder")
-      .def(pybind11::init<int, int, int, uint32_t, py::array_t<int64_t>,
-                          py::array_t<float>, py::array_t<int32_t>,
-                          py::array_t<int32_t>>())
-      .def("decode", &HyperIBLTDecoder::decode, py::arg{}.noconvert());
+  m.def("hyperiblt_decode", &hyperiblt_decode);
 }
